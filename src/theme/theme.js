faefa2843a1139dc0aaa11b1c13a08ba936cb645
Adrian Rudnicki <adi2602@wp.pl>
2022-10-17 22:32:25 +0200

    Create theme file for MUI

---
+++ src/theme/theme.js
@@ -0,0 +1,17 @@
+export const customTheme = {
+  palette: {
+    primary: {
+      main: "#F6F6F6",
+      light: "#FDFDFD",
+      dark: "#C4C4C4",
+    },
+    text: {
+      secondary: "#C0FCFF",
+    },
+    secondary: {
+      main: "#4ca0a5",
+      light: "#C0FCFF",
+      dark: "#1e8388",
+    },
+  },
+};
