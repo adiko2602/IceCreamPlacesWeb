7b4fc603f4a06a9f7ff2ec9febdc92c5db89f2fa
Adrian Rudnicki <adi2602@wp.pl>
2022-10-18 12:33:31 +0200

    Consume Register

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      <div style={{ marginTop: "2rem" }}></div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
