ececf4fe34a00c57bd52b13e0ac72a2f2e4c6de0
Adrian Rudnicki <adi2602@wp.pl>
2022-10-18 12:33:00 +0200

    Some theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -5,9 +5,6 @@ export const customTheme = {
       light: "#FDFDFD",
       dark: "#C4C4C4",
     },
-    text: {
-      secondary: "#C0FCFF",
-    },
     secondary: {
       main: "#4ca0a5",
       light: "#C0FCFF",
