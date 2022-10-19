98e85d2d6059d47efdcca0a982235ea0d3c3ab7d
Adrian Rudnicki <adi2602@wp.pl>
2022-10-19 17:45:17 +0200

    Create Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,73 @@
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import ShopCard from "../components/ShopCard";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  // fetch data
+  const shops = [
+    {
+      name: "Choise",
+      address: "Legnica, Piastowska 2A/2",
+      iceCreams: ["trusk", "czekol"],
+    },
+    {
+      name: "Lody Naturalne",
+      address: "Legnica, Chojnowska 27",
+      iceCreams: ["malin", "cist"],
+    },
+    {
+      name: "Choise",
+      address: "Legnica, Piastowska 2A/2",
+      iceCreams: ["trusk", "czekol"],
+    },
+    {
+      name: "Lody Naturalne",
+      address: "Legnica, Chojnowska 27",
+      iceCreams: ["malin", "cist"],
+    },
+    {
+      name: "Choise",
+      address: "Legnica, Piastowska 2A/2",
+      iceCreams: ["trusk", "czekol"],
+    },
+    {
+      name: "Lody Naturalne",
+      address: "Legnica, Chojnowska 27",
+      iceCreams: ["malin", "cist"],
+    },
+    {
+      name: "Choise",
+      address: "Legnica, Piastowska 2A/2",
+      iceCreams: ["trusk", "czekol"],
+    },
+    {
+      name: "Lody Naturalne",
+      address: "Legnica, Chojnowska 27",
+      iceCreams: ["malin", "cist"],
+    },
+  ];
+
+  return (
+    <Box>
+      <Typography variant="h5" gutterBottom>
+        Witaj na stronie poświęconej lodziarniom. Tutaj znajdziesz każdą
+        lodziarnię w Twojej okolicy.
+      </Typography>
+      <Grid
+        container
+        gap={3}
+        direction="row"
+        justifyContent="center"
+        alignItems="flex-start"
+      >
+        {shops.map((shop) => (
+          <Grid item xs={12} sm={6} md={4} lg={3}>
+            <ShopCard shop={shop} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default Home;
