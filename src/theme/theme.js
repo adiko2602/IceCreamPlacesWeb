25832b488a4111fe48492a717f0d26aeb887abad
Adrian Rudnicki <adi2602@wp.pl>
2022-10-19 17:45:50 +0200

    Theme change

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,14 +1,24 @@
 export const customTheme = {
   palette: {
+    type: "light",
     primary: {
-      main: "#F6F6F6",
-      light: "#FDFDFD",
-      dark: "#C4C4C4",
+      main: "#272727",
     },
     secondary: {
-      main: "#4ca0a5",
-      light: "#C0FCFF",
-      dark: "#1e8388",
+      main: "#717171",
     },
+    background: {
+      default: "#f5f5f7",
+    },
+    text: {
+      primary: "rgba(29,29,31,0.85)",
+      secondary: "rgba(162, 162, 170, 0.85)",
+      disabled: "rgba(29,29,31,0.5)",
+      hint: "rgba(107,107,111,0.5)",
+    },
+  },
+  typography: {
+    htmlFontSize: 20,
+    fontFamily: "Montserrat",
   },
 };
