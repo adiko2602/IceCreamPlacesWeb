10048f00649f56a9a14989d9091b0af230bd4fea
Adrian Rudnicki <adi2602@wp.pl>
2022-10-21 12:12:26 +0200

    Update to use Context

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import "./index.css";
 import App from "./App";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { customTheme } from "./theme/theme";
+import { UserContextProvider } from "./context/UserContext";
+import { GlobalContextProvider } from "./context/GlobalContext";
 
 const theme = createTheme(customTheme);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <GlobalContextProvider>
+      <UserContextProvider>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </UserContextProvider>
+    </GlobalContextProvider>
   </React.StrictMode>
 );
