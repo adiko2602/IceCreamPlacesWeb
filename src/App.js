b15cfadb12435e94dd69411692d02174dfb2badd
Adrian Rudnicki <adi2602@wp.pl>
2022-10-28 18:42:28 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -33,24 +33,22 @@ const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <div className="relative">
-        {/* {global.isLoading && <Loading />} */}
-        <div className="container">
-          <div className="max-width flex-col">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/shop/:id" element={<Shop />} />
-              <Route exact path="/shop/add" element={<ShopAdd />} />
-              <Route
-                path="/profile"
-                element={user.type === "default" ? <Lost /> : <Profile />}
-              />
-              <Route path="/login" element={<Login />} />
-              <Route path="/test" element={<Test />} />
-            </Routes>
-          </div>
+      {/* {global.isLoading && <Loading />} */}
+      <div className="container">
+        <div className="max-width flex-col">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/shop/:id" element={<Shop />} />
+            <Route exact path="/shop/add" element={<ShopAdd />} />
+            <Route
+              path="/profile"
+              element={user.type === "default" ? <Lost /> : <Profile />}
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/test" element={<Test />} />
+          </Routes>
         </div>
       </div>
     </BrowserRouter>
--- src/reducers/globalReducer.js
+++ src/reducers/globalReducer.js
@@ -3,9 +3,18 @@ export const globalReducer = (state, action) => {
     case "SET_IS_MOBILE":
       return {
         global: {
+          ...state.global,
           isMobile: action.payload,
         },
       };
+    case "SET_IS_LOADING":
+      return {
+        global: {
+          ...state.global,
+          isLoading: action.payload,
+        },
+      };
+
     default:
       return state;
   }
