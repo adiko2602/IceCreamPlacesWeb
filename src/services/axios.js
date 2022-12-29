73881836a8dc6d803fa88249cb439a8426ce03a8
Adrian Rudnicki <adi2602@wp.pl>
2022-12-29 13:32:42 +0100

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseUrl =
-  "https://ice-cream-places-api-git-old-dev-refactor-ice-cream-places-icp.vercel.app/";
+const baseUrl = "https://https://ice-cream-places-api.vercel.app/";
 
 export const useAxios = () => {
   return axios.create({
