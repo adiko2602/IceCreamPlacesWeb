e60391c88ce60950f55a18967afd37a93c15ce6d
Adrian Rudnicki <adi2602@wp.pl>
2023-01-06 02:12:48 +0100

    Update LoginGoogle.js

--- src/pages/LoginGoogle.js
+++ src/pages/LoginGoogle.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useUser } from "../context/UserContext";
 import { CheckIfLogin, LoginWithGoogle } from "../services/auth";
+import { baseUrl } from "../services/axios";
 import { GetUser } from "../services/user";
 
 const LoginGoogle = () => {
@@ -11,25 +12,27 @@ const LoginGoogle = () => {
   const userContext = useUser();
 
   useEffect(() => {
-    const log = async () => {
-      const dataLoginWithGoogle = await LoginWithGoogle();
-      if (!dataLoginWithGoogle.status) {
-        setError(dataLoginWithGoogle.message);
-        return;
-      }
-      if (await CheckIfLogin()) {
-        const userData = await GetUser();
-        if (!userData.status) {
-          setError(userData.message);
-          return;
-        }
-        await userContext.setUser(userData.content);
-      }
-    };
+    // const log = async () => {
+    //   const dataLoginWithGoogle = await LoginWithGoogle();
+    //   if (!dataLoginWithGoogle.status) {
+    //     setError(dataLoginWithGoogle.message);
+    //     return;
+    //   }
+    //   if (await CheckIfLogin()) {
+    //     const userData = await GetUser();
+    //     if (!userData.status) {
+    //       setError(userData.message);
+    //       return;
+    //     }
+    //     await userContext.setUser(userData.content);
+    //   }
+    // };
+
+    window.open(`${baseUrl}/auth/google/success`, "_self");
 
     navigate("/");
 
-    log();
+    // log();
   }, []);
 
   return <div>{error && <div className="error">{error}</div>}</div>;
