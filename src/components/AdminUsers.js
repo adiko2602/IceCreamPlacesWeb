b4a53d8b351e725d10425c318a4078d527ff06a3
Adrian Rudnicki <adi2602@wp.pl>
2023-01-09 20:52:29 +0100

    Add search text box

--- src/components/AdminUsers.js
+++ src/components/AdminUsers.js
@@ -4,11 +4,10 @@ import {
   Typography,
   CardContent,
   Link as MuiLink,
+  TextField,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { GetShops } from "../services/shop";
-import { ColorRing } from "react-loader-spinner";
 import Loading from "./Loading";
 import { GetAllUsers } from "../services/user";
 
@@ -16,6 +15,8 @@ const AdminUsers = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
   const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState("");
+  const [filteredUser, setFilteredUser] = useState([]);
 
   useEffect(() => {
     setError("");
@@ -35,6 +36,23 @@ const AdminUsers = () => {
     populateUsers();
   }, []);
 
+  useEffect(() => {
+    if (query === "") {
+      setFilteredUser([]);
+    } else {
+      setFilteredUser(
+        users.filter((user) => {
+          if (
+            user.email.toLowerCase().includes(query.toLowerCase()) ||
+            user._id.toLowerCase().includes(query.toLowerCase())
+          )
+            return user;
+          return null;
+        })
+      );
+    }
+  }, [query, users]);
+
   if (loading) return <Loading />;
 
   if (!users) return <div>Brak Użytkowników w bazie</div>;
@@ -47,18 +65,42 @@ const AdminUsers = () => {
       />
       {error && <div className="error">{error}</div>}
       <CardContent className="card-content">
-        {users.map((user) => (
-          <div
-            key={user._id}
-            style={{ borderBottom: "solid 1px black", padding: "10px 0" }}
-          >
-            <MuiLink component={Link} to={`/admin/users/${user._id}`}>
-              {user._id}
-              <br />
-              <strong>{user.email}</strong>
-            </MuiLink>
-          </div>
-        ))}
+        <TextField
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          type="text"
+          label="Szukaj"
+          fullWidth
+        />
+      </CardContent>
+      <CardContent className="card-content">
+        {filteredUser.length <= 0 &&
+          users.map((user) => (
+            <div
+              key={user._id}
+              style={{ borderBottom: "solid 1px black", padding: "10px 0" }}
+            >
+              <MuiLink component={Link} to={`/admin/users/${user._id}`}>
+                {user._id}
+                <br />
+                <strong>{user.email}</strong>
+              </MuiLink>
+            </div>
+          ))}
+
+        {filteredUser.length > 0 &&
+          filteredUser.map((user) => (
+            <div
+              key={user._id}
+              style={{ borderBottom: "solid 1px black", padding: "10px 0" }}
+            >
+              <MuiLink component={Link} to={`/admin/users/${user._id}`}>
+                {user._id}
+                <br />
+                <strong>{user.email}</strong>
+              </MuiLink>
+            </div>
+          ))}
       </CardContent>
     </Card>
   );
